685013a159d58053c3eb97d9a904f44391ac60b4
Tobias Artinian <86538603+tobiartinian@users.noreply.github.com>
2022-02-27 13:21:37 -0300

    Add files via upload

---
+++ SQL-ETL/INSERCION 21-12-2021 23-12-2021.sql
@@ -0,0 +1,56 @@
+--INSERCIÓN DE EJERCICIOS
+INSERT INTO EJERCICIOS VALUES (9,'SENTADILLA','PIERNA');
+INSERT INTO EJERCICIOS VALUES (10,'PRESS MILITAR','HOMBRO');
+INSERT INTO EJERCICIOS VALUES (11,'HIP TRUST','PIERNA');
+INSERT INTO EJERCICIOS VALUES (12,'PRENSA','PIERNA');
+INSERT INTO EJERCICIOS VALUES (13,'CURL FEMORAL','PIERNA');
+INSERT INTO EJERCICIOS VALUES (14,'FACEPULL','HOMBRO');
+INSERT INTO EJERCICIOS VALUES (15,'VUELOS LATERALES','HOMBRO');
+INSERT INTO EJERCICIOS VALUES (16,'PRESS JAVALINA','HOMBRO');
+
+-- INSERCIÓN DE ENTRENAMIENTO 2021-12-21
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (2,9,'2021-12-21',3,5,60,7,1);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (2.1,10,'2021-12-21',3,8,30,6,1);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (2.2,12,'2021-12-21',3,10,80,9,4);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (2.3,13,'2021-12-21',2,20,35,8,1);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (2.4,14,'2021-12-21',3,12,17.5,7.5,4);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (2.5,15,'2021-12-21',3,15,7.5,7.5,2);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (2.6,16,'2021-12-21',2,20,10,6,0)
+
+--INSERCIÓN DE EJERCICIOS
+INSERT INTO EJERCICIOS VALUES (17,'PRESS BANCA','PECHO');
+INSERT INTO EJERCICIOS VALUES (18,'PRESS INCLINADO','PECHO');
+INSERT INTO EJERCICIOS VALUES (19,'APERTURA CON MANCUERNA','PECHO');
+INSERT INTO EJERCICIOS VALUES (20,'CRUCES CON  POLEA BAJA','PECHO');
+INSERT INTO EJERCICIOS VALUES (21,'CURL DE BICEPS BARRA Z','BICEPS');
+INSERT INTO EJERCICIOS VALUES (22,'CURL DE BICEPS MARTILLO','BICEPS');
+INSERT INTO EJERCICIOS VALUES (23,'CURLS DE BICEPS CONCENTRADO','BICEPS');
+
+-- INSERCIÓN DE ENTRENAMIENTOS 2021-12-23
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (3,17,'2021-12-23',1,4,70,6,3);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (3.1,17,'2021-12-23',2,8,70,7,-1);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (3.2,18,'2021-12-23',2,6,50,2,2);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (3.3,18,'2021-12-23',1,9,40,3,3);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (3.4,19,'2021-12-23',3,10,10,7,3);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (3.5,20,'2021-12-23',3,15,10,6,3);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (3.6,21,'2021-12-23',3,8,5,8,5);
+INSERT INTO ENTRENAMIENTOS(n_ejercicio,ejercicio_id,fecha,series,repeticiones,peso,sensacion,RIR) VALUES (3.7,22,'2021-12-23',3,10,12.5,8,2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
